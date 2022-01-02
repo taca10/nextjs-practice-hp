89c02ca3b1055d55a0b2800ca6274fc291069b48
坪内崇人 <taka.ur.0505@gmail.com>
2022-01-02 14:51:43 +0900

    ホーム画面のみ表示

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,56 +1,56 @@
-import Link from "next/link";
-import Layout from "../../components/Layout";
-import { getAllPostsIds, getPostData } from "../../lib/posts";
+// import Link from "next/link";
+// import Layout from "../../components/Layout";
+// import { getAllPostsIds, getPostData } from "../../lib/posts";
 
-export default function Post({ post }) {
-  if (!post) {
-    return <div>Loading</div>;
-  }
+// export default function Post({ post }) {
+//   if (!post) {
+//     return <div>Loading</div>;
+//   }
 
-  return (
-    <Layout title={post.title}>
-      <p className="m-4">
-        {"ID: "}
-        {post.id}
-      </p>
-      <p className="mb-8 text-xl font-bold">{post.title}</p>
-      <p className="px-10">{post.body}</p>
-      <Link href="/blog-page">
-        <div className="flex cursor-pointer mt-12">
-          <svg
-            className="w-6 h-6 mr-3"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
-            ></path>
-          </svg>
-          <span>Back to blog-page</span>
-        </div>
-      </Link>
-    </Layout>
-  );
-}
+//   return (
+//     <Layout title={post.title}>
+//       <p className="m-4">
+//         {"ID: "}
+//         {post.id}
+//       </p>
+//       <p className="mb-8 text-xl font-bold">{post.title}</p>
+//       <p className="px-10">{post.body}</p>
+//       <Link href="/blog-page">
+//         <div className="flex cursor-pointer mt-12">
+//           <svg
+//             className="w-6 h-6 mr-3"
+//             fill="none"
+//             stroke="currentColor"
+//             viewBox="0 0 24 24"
+//             xmlns="http://www.w3.org/2000/svg"
+//           >
+//             <path
+//               strokeLinecap="round"
+//               strokeLinejoin="round"
+//               strokeWidth="2"
+//               d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+//             ></path>
+//           </svg>
+//           <span>Back to blog-page</span>
+//         </div>
+//       </Link>
+//     </Layout>
+//   );
+// }
 
-export async function getStaticPaths() {
-  const paths = await getAllPostsIds();
-  return {
-    paths,
-    fallback: false,
-  };
-}
+// export async function getStaticPaths() {
+//   const paths = await getAllPostsIds();
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
 
-export async function getStaticProps({ params }) {
-  const { post: post } = await getPostData(params.id);
-  return {
-    props: {
-      post,
-    },
-  };
-}
+// export async function getStaticProps({ params }) {
+//   const { post: post } = await getPostData(params.id);
+//   return {
+//     props: {
+//       post,
+//     },
+//   };
+// }
