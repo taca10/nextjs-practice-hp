f39fbd6b5e3d460d7ecd830f802415bafb15e30f
坪内崇人 <taka.ur.0505@gmail.com>
2022-01-02 15:19:59 +0900

    デプロイ時のwarning解消

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,25 +1,20 @@
-// import Layout from "../components/Layout";
-// import Post from "../components/Post";
-// import { getAllPostsData } from "../lib/posts";
+import Layout from "../components/Layout";
+import Post from "../components/Post";
+import { getAllPostsData } from "../lib/posts";
 
-// const Blog = ({ posts }) => {
-//   return (
-//     <Layout title="Blog">
-//       Blog page
-//       <ul className="m-10">
-//         {posts && posts.map((post) => <Post key={post.id} post={post} />)}
-//         {/* {posts && posts.map((post) => <Post key={post.id} post={post}> /> )} */}
-//       </ul>
-//     </Layout>
-//   );
-// };
+export default function Blog({ posts }) {
+  return (
+    <Layout title="Blog">
+      <ul className="m-10">
+        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
+    </Layout>
+  );
+}
 
-// export default Blog;
-
-// export async function getStaticProps() {
-//   // build時にサーバーサイドで実行される。
-//   const posts = await getAllPostsData();
-//   return {
-//     props: { posts },
-//   };
-// }
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+  return {
+    props: { posts },
+  };
+}
