933e81c19992f0173a9a5e2b31a8ae1b0a0d7098
坪内崇人 <taka.ur.0505@gmail.com>
2022-01-02 15:07:16 +0900

    デプロイ時のwarning解消

--- components/Post.js
+++ components/Post.js
@@ -5,7 +5,7 @@ const Post = ({ post }) => {
     <div>
       <span>{post.id}</span>
       {" : "}
-      <Link href={`/posts/${post.id}`}>
+      <Link href={`/posts/${post.id}`} passHref>
         <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
           {post.title}
         </span>
