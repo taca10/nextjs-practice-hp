a13fc2b83737c6cfc29e929399d7b6184a29d48b
坪内崇人 <taka.ur.0505@gmail.com>
2022-01-02 19:05:20 +0900

    Post.js修正

--- components/Post.js
+++ components/Post.js
@@ -1,17 +1,14 @@
 import Link from "next/link";
-
-const Post = ({ post }) => {
+export default function Post({ post }) {
   return (
     <div>
       <span>{post.id}</span>
       {" : "}
-      <Link href={`/posts/${post.id}`} passHref>
+      <Link href={`/posts/${post.id}`}>
         <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
           {post.title}
         </span>
       </Link>
     </div>
   );
-};
-
-export default Post;
+}
