a5421286487d420c97e1f9732be119a7a41be016
坪内崇人 <taka.ur.0505@gmail.com>
2022-01-02 15:34:07 +0900

    色々なファイルをコメントアウト

--- components/Post.js
+++ components/Post.js
@@ -1,17 +1,17 @@
-import Link from "next/link";
+// import Link from "next/link";
 
-const Post = ({ post }) => {
-  return (
-    <div>
-      <span>{post.id}</span>
-      {" : "}
-      <Link href={`/posts/${post.id}`} passHref>
-        <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
-          {post.title}
-        </span>
-      </Link>
-    </div>
-  );
-};
+// const Post = ({ post }) => {
+//   return (
+//     <div>
+//       <span>{post.id}</span>
+//       {" : "}
+//       <Link href={`/posts/${post.id}`} passHref>
+//         <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
+//           {post.title}
+//         </span>
+//       </Link>
+//     </div>
+//   );
+// };
 
-export default Post;
+// export default Post;
